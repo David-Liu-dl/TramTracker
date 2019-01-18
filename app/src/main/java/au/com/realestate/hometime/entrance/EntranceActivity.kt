0f951e5d7ce6aeee0d419a3b5608dd9a9f99818c
davidfancy <lyhmelbourne@gmail.com>
2019-01-19 10:31:55 +1100

    add material theme for HomeTime

--- app/src/main/java/au/com/realestate/hometime/entrance/EntranceActivity.kt
+++ app/src/main/java/au/com/realestate/hometime/entrance/EntranceActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import au.com.realestate.hometime.R
 
-class MainActivity: AppCompatActivity(){
+class EntranceActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/au/com/realestate/hometime/tramtimetable/TramTimetableActivity.kt
@@ -0,0 +1,7 @@
+package au.com.realestate.hometime.tramtimetable
+
+import androidx.appcompat.app.AppCompatActivity
+
+class TramTimetableActivity: AppCompatActivity(){
+
+}
