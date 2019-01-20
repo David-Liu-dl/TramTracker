eb9d16fe5ba24456028cac2f1ccb9d03897cc6da
davidfancy <lyhmelbourne@gmail.com>
2019-01-20 14:10:14 +1100

    fix: time remains bug

--- library/core-infra/src/main/java/au/com/realestate/infra/extensions/DateExtensions.kt
+++ library/core-infra/src/main/java/au/com/realestate/infra/extensions/DateExtensions.kt
@@ -3,11 +3,8 @@ package au.com.realestate.infra.extensions
 import java.util.*
 
 fun Date.diffWithCurrentTimeInMinutes(): Long {
-    val diff = Date().time - this.time
+    val diff = this.time - Date().time
     val seconds = diff / 1000
-    val minutes = seconds / 60
-    val hours = minutes / 60
-    val days = hours / 24
 
-    return minutes
+    return seconds / 60
 }
